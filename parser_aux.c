67e57a9d422015f29622dbd3ccf8c80eac412222
Gabriel Vargas <gvargasr@estudiantec.cr>
2021-03-22 18:40:03 -0600

    finished parser_aux functions

--- parser_aux.c
+++ parser_aux.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "headers/tokens.h"
+#include "headers/scanner.h"
 
-void match(token t)
-{
-    // TODO implementar
-};
+token current_token;
 
 token next_token()
 {
-    return ID; //TODO implementar bien
+    current_token = scanner();
+    return current_token; //TODO implementar bien
 };
 
 void syntax_error(token t)
@@ -17,4 +16,11 @@ void syntax_error(token t)
     //TODO: implementar bien. Este código es temporal
     printf("Syntax error\n");
     exit(1); //TODO: revisar exit codes
+};
+
+void match(token t)
+{
+    if (t != next_token()){
+        syntax_error(t);
+    }
 };
