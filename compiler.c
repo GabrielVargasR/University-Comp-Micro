d623e167ae9be9167b23921d35006cf67aaaa3be
Gabriel Vargas <gvargasr@estudiantec.cr>
2021-03-22 19:20:16 -0600

    added file validation

--- compiler.c
+++ compiler.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 #include "headers/parser.h"
+#define SOURCE_CODE_EXTENSION "micro"
+
+bool valid_file_exists(char path[]);
 
 int main(int argc, char *argv[])
 {
     if (argc == 2) {
-        printf("%s\n", argv[1]);
+        // printf("%s\n", argv[1]);
+        if (valid_file_exists(argv[1])){
+            // open file
+        } else {
+            printf("The specified file was not found or was not a .micro file");
+            return 1;
+        }
     } else {
         printf("\nExpected (1) argument, got (%d)\n", argc-1);
         printf("Usage: comp <path/to/micro/file>\n");
     }
     return 0;
+}
+
+bool valid_file_exists(char path[])
+{
+    char *extension = strrchr(path, '.');
+    extension = extension+1;
+
+    if (access(path, R_OK) == 0 && !(strcmp(extension, SOURCE_CODE_EXTENSION))) {
+        return true;
+    }
+    return false;
 }
