70f2edb043813e6799b57504d55341f8d85188ea
Gabriel Vargas <gvargasr@estudiantec.cr>
2021-03-29 22:39:30 -0600

    left code ready for parser modification

---
+++ headers/action_routines.h
@@ -0,0 +1,15 @@
+#ifndef ACTION_ROUTINES_H
+#define ACTION_ROUTINES_H
+#include "records.h"
+
+void start(void);
+void finish(void);
+void assign(expr_rec target, expr_rec source);
+op_rec process_op(void);
+expr_rec gen_infix(expr_rec e1, op_rec op, expr_rec e2);
+void read_id(expr_rec in_var);
+expr_rec process_id(void);
+expr_rec process_literal(void);
+void write_expr(expr_rec out_expr);
+
+#endif
---
+++ ids.c
@@ -0,0 +1,23 @@
+#include "headers/symbol_table.h"
+#include "headers/generator.h"
+#include <stdio.h>
+
+void check_id(string s)
+{
+    if (! lookup(s)){
+        enter(s);
+        generate("Declare", s, "Integer", "");
+    }
+}
+
+
+char *get_temp(void)
+{
+    static int max_temp = 0;
+    static char tempname[MAX_ID_LEN];
+
+    max_temp++;
+    sprintf(tempname, "Temp&%d", max_temp);
+    check_id(tempname);
+    return tempname;
+}
