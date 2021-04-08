2a57fa0951dadf49930afc095aae0ec854da83fa
Gabriel Vargas <gvargasr@estudiantec.cr>
2021-04-08 12:43:21 -0600

    added if to syntax

--- headers/parser.h
+++ headers/parser.h
@@ -1,13 +1,14 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+int conditional_flag;
 void system_goal();
 void program();
 void statement_list();
 void statement();
 void id_list();
-void normal_expression();
 void expression();
+void conditional();
 void expr_list();
 void add_op();
 void primary();
